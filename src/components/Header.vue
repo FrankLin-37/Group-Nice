7b0c419aa21ac9f3f8378fa00d1ef018f10fbcfe
Lokuda1005 <u220a503@kyowa-u.ac.jp>
2024-07-17 16:12:05 +0800

    time2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,6 @@ export default{
 
 <template>
         <div class="headerArea">
-
     <RouterLink class="routeItem" to="/">One</RouterLink>
     <RouterLink class="routeItem" to="/Two">Two</RouterLink>
     <RouterLink class="routeItem" to="/Three">贊助我們</RouterLink>
@@ -20,9 +19,20 @@ export default{
 </template>
 
 <style scoped lang="scss">
-.headerArea{
+.headerArea{    
     width: 100%;
-    height: 100%;
-    border: 1.5px solid black;
+    height: 9dvh;
+    display: flex;
+    align-items: center;
+    font-size: 21px;
+    background-color: #8A8675;
+    transition: 0.5s;
+    position: fixed;
+    z-index: 1;
+    .routeItem{
+        font-size: 24px;
+        padding-left: 8%;
+        color: aliceblue;
+    }
 }
 </style>
